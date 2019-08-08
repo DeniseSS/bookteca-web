86eeab44fd21fa7fa1c274a71b22be5ef678af15
DENISE SANTOS DE SOUZA <540317706@alunos.etec.itaqua>
2019-08-07 22:31:36 -0300

    validaçoes de formulario

--- livro.js
+++ livro.js
@@ -1,5 +1,5 @@
 window.onload= () =>{
-    let elementoBotao = document.querySelector('#cadastrar')
+ /*   let elementoBotao = document.querySelector('#cadastrar')
 elementoBotao.addEventListener('click', (event)=>{
     event.preventDefault()
     console.log('Clicou no botão')
@@ -15,6 +15,45 @@ elementoBotao.addEventListener('click', (event)=>{
             ladda.stop()
         }
     }, 500)
+})*/
+let formularioLivro=$('#formLivro')
+formularioLivro.validate({
+    /*regras*/
+    rules:{
+        titulo:{
+            required:true,
+            minlength:3
+        },
+        subtitulo:{
+            
+            minlength:3
+        },
+        descricao:{
+            required:true,
+            minlength:3
+        }
+    },
+    /*para cada parametro uma resposta referente a ele*/ 
+    messages:{
+        titulo:{
+            required:'O titulo é obrigatório!',
+            minlength:'O titulo é muito curto!'
+        },
 
+        subtitulo:{
+            required:'O subtitulo é obrigatório!',
+          minlength:'subtitulo muito curto!'
+        },
+        descricao:{
+            required:'A descrição é obrigatória!',
+            minlength:'descrição muito curta!'
+        }
+    },
+    errorPlacement:function(error, element){
+         element.parent().parent().find('.error').append(error).addClass('animated shake')//navegando para buscar a class
+
+    }, 
+    errorClass:'cor-do-erro'
 })
+
 }
